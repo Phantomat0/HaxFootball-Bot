f77779b7e14773db9dc846bad409857289521dcf
Phantomat0 <soldierxfreedom@gmail.com>
2022-04-27 11:39:08 -0500

    first commit

---
+++ src/server/index.ts
@@ -0,0 +1,59 @@
+import puppeteer from "puppeteer";
+
+const HAXBALL_HEADLESS_URL = "https://www.haxball.com/headless";
+
+(async () => {
+  const IS_HEADLESS = false;
+
+  const browser = await puppeteer.launch({ headless: IS_HEADLESS });
+
+  const roomLink = await loadRoomAndGetURL(browser);
+
+  if (IS_HEADLESS) return;
+  await joinRoom(browser, roomLink, "GAMMA RAY12344");
+})();
+
+async function loadRoomAndGetURL(browser: puppeteer.Browser): Promise<string> {
+  const haxballAPIPage = await browser.newPage();
+  await haxballAPIPage.goto(HAXBALL_HEADLESS_URL, {
+    waitUntil: "networkidle2",
+  });
+
+  await haxballAPIPage.addScriptTag({ path: "dist/app-bundle.js" });
+
+  const haxballAPIIframe = haxballAPIPage
+    .frames()
+    .find((frame) => frame.parentFrame() !== null);
+
+  // look for room url within frame
+
+  const roomLinkTag = await haxballAPIIframe.waitForSelector("#roomlink a");
+
+  const roomLink = await roomLinkTag.getProperty("href");
+
+  return await roomLink.jsonValue();
+}
+
+async function joinRoom(
+  browser: puppeteer.Browser,
+  roomLink: string,
+  playerName: string
+) {
+  const haxballPlayPage = await browser.newPage();
+
+  await haxballPlayPage.goto(roomLink, {
+    waitUntil: "networkidle2",
+  });
+
+  const haxballPlayPageIframe = haxballPlayPage
+    .frames()
+    .find((frame) => frame.parentFrame() !== null);
+  await haxballPlayPageIframe.waitForSelector("div.label-input input");
+  await haxballPlayPageIframe.focus("div.label-input input");
+  await haxballPlayPage.keyboard.type(playerName);
+
+  await haxballPlayPageIframe.$eval(
+    "div.dialog button",
+    (el: HTMLButtonElement) => el.click()
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  target: "node",
+  entry: {
+    room: "./src/room/index.ts",
+    server: "./src/server/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]-bundle.js",
+  },
+  watch: true,
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        // include: [path.resolve(__dirname, "src/room")],
+      },
+    ],
+  },
+};
