d0990fa47dd4ad80f805bf41f4f29aac649f8f06
Phantomat0 <soldierxfreedom@gmail.com>
2022-05-12 19:49:04 -0500

    special message formatter for down and distance

---
+++ src/room/structures/DownAndDistanceFormatter.ts
@@ -0,0 +1,39 @@
+import { Position } from "../HBClient";
+import { MAP_POINTS } from "../utils/map";
+
+class DownAndDistanceFormatter {
+  formatLOSDistance() {}
+
+  formatRedZonePenalties(currentRedZonePenalties: 0 | 1 | 2 | 3) {
+    if (currentRedZonePenalties === 0) return ""; // If there are no redzone penalties
+    return ` [${currentRedZonePenalties}/3]`;
+  }
+
+  formatDown(currentDown: 1 | 2 | 3 | 4 | 5) {
+    const downStrings = {
+      1: "1st",
+      2: "2nd",
+      3: "3rd",
+      4: "4th",
+      5: "5th",
+    };
+    return downStrings[currentDown];
+  }
+
+  formatYardsToGain(lineToGainPoint: Position["x"], lineToGainYards: number) {
+    if (
+      lineToGainPoint <= MAP_POINTS.RED_ENDZONE ||
+      lineToGainPoint >= MAP_POINTS.BLUE_ENDZONE
+    )
+      return "GOAL";
+    return lineToGainYards;
+  }
+
+  formatPositionToMapHalf = (x: Position["x"]) => {
+    if (x > MAP_POINTS.KICKOFF) return "BLUE ";
+    if (x < MAP_POINTS.KICKOFF) return "RED ";
+    return "";
+  };
+}
+
+export default new DownAndDistanceFormatter();
