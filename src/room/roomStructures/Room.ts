3b55df8b321f94f36ffabae0a9617bfdebbedb3c
Phantomat0 <soldierxfreedom@gmail.com>
2022-05-21 15:48:35 -0500

    startNewGame on game start

--- src/room/roomStructures/Room.ts
+++ src/room/roomStructures/Room.ts
@@ -1,11 +1,12 @@
 import Game from "../classes/Game";
 import HBClient from "../HBClient";
 import { PLAY_TYPES } from "../plays/BasePlay";
+import KickOff from "../plays/Kickoff";
 import PlayerManager from "./Players";
 
 export default class RoomClient {
   readonly client: HBClient;
-  readonly game: Game = new Game();
+  game: Game;
   readonly players: PlayerManager = new PlayerManager();
   private _isBotOn: boolean = true;
 
@@ -23,5 +24,10 @@ export default class RoomClient {
   getPlay<T extends PLAY_TYPES>() {
     if (!this.game.play) throw new Error("Game Error: Play is not defined");
     return this.game.play as T;
+  }
+
+  startNewGame() {
+    this.game = new Game();
+    this.game.setPlay(new KickOff(0), null);
   }
 }
