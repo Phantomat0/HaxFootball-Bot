ba9d0b9165b4d5a5e1d3d1633b901c315cbff768
Phantomat0 <soldierxfreedom@gmail.com>
2022-05-25 22:08:35 -0500

    field goals

--- src/room/plays/Fieldgoal.ts
+++ src/room/plays/Fieldgoal.ts
@@ -1,209 +1,238 @@
+import Room, { client } from "..";
 import BallContact from "../classes/BallContact";
-import { Position } from "../HBClient";
+import PlayerContact from "../classes/PlayerContact";
+import { PlayerObject, Position } from "../HBClient";
+import Chat from "../roomStructures/Chat";
+import DistanceCalculator from "../structures/DistanceCalculator";
+import GameReferee from "../structures/GameReferee";
+import MapReferee from "../structures/MapReferee";
+import ICONS from "../utils/Icons";
+import { MAP_POINTS } from "../utils/map";
 import FieldGoalEvents from "./play_events/FieldGoal.events";
 
 export default class FieldGoal extends FieldGoalEvents {
-  handleTouchdown(position: Position): void {}
+  private _kicker: PlayerObject;
 
-  protected _handleBallContactDefense(ballContactObj: BallContact): void {}
+  handleTouchdown(endPosition: Position): void {
+    const { netYards } = this._getPlayDataOffense(endPosition);
 
-  protected _handleBallContactOffense(ballContactObj: BallContact): void {}
+    // Only way a TD can be scored is if its a rushing TD
+    Room.game.stats.updatePlayerStat(this._ballCarrier?.id!, {
+      rushingAttempts: 1,
+      rushingYards: netYards,
+      touchdownsRushed: 1,
+    });
 
-  onKickDrag(): void {}
+    super.handleTouchdown(endPosition);
+  }
+
+  protected _getKicker() {
+    return this._kicker;
+  }
+
+  constructor(time: number, kicker: PlayerObject) {
+    super(time);
+    this._kicker = kicker;
+    this._ballCarrier = kicker;
+  }
+
+  protected _setKickerInPosition() {
+    const sevenYardsBehindBall = new DistanceCalculator()
+      .subtractByTeam(
+        Room.game.down.getSnapPosition().x,
+        MAP_POINTS.YARD * 7,
+        Room.game.offenseTeamId
+      )
+      .calculate();
+
+    client.setPlayerDiscProperties(this._kicker.id, {
+      x: sevenYardsBehindBall,
+      y: MAP_POINTS.TOP_HASH,
+    });
+
+    return this;
+  }
+
+  protected _setOffenseInPosition() {
+    const offensePlayers = Room.game.players.getOffense();
+
+    const opposingEndzone = MapReferee.getOpposingTeamEndzone(
+      Room.game.offenseTeamId
+    );
+
+    const fiveYardsBeforeEndzone = new DistanceCalculator()
+      .subtractByTeam(
+        opposingEndzone,
+        MAP_POINTS.YARD * 5,
+        Room.game.offenseTeamId
+      )
+      .calculate();
+
+    offensePlayers.forEach(({ id }) => {
+      // Dont set the kicker's position, we already do that
+      if (id === this._kicker.id) return;
+      client.setPlayerDiscProperties(id, { x: fiveYardsBeforeEndzone });
+    });
+
+    return this;
+  }
+
+  protected _setDefenseInPosition() {
+    const defensePlayers = Room.game.players.getDefense();
+    const opposingEndzone = MapReferee.getOpposingTeamEndzone(
+      Room.game.offenseTeamId
+    );
+
+    const oneYardInFrontOfEndzone = new DistanceCalculator()
+      .subtractByTeam(
+        opposingEndzone,
+        MAP_POINTS.YARD * 1,
+        Room.game.defenseTeamId
+      )
+      .calculate();
+
+    defensePlayers.forEach(({ id }) => {
+      client.setPlayerDiscProperties(id, { x: oneYardInFrontOfEndzone });
+    });
+
+    return this;
+  }
+
+  handleSuccessfulFg(msg: string) {
+    Chat.send(msg);
+    this.scorePlay(3, Room.game.offenseTeamId, Room.game.defenseTeamId);
+  }
+
+  handleUnsuccessfulFg(msg: string) {
+    Chat.send(msg);
+    this.endPlay({ resetDown: true });
+  }
+
+  protected _setPlayersInPosition() {
+    this._setKickerInPosition()._setDefenseInPosition()._setOffenseInPosition();
+  }
+
+  protected _handleBallContactDefense(ballContactObj: BallContact): void {
+    // If the field goal was kicked, and it was kicked before the blitz and they touched it
+
+    // If the FG was kicked
+    if (this.stateExists("fieldGoalKicked")) {
+      console.log(this.readAllState());
+      // If it was kicked before the blitz, its a penalty, if it was after, its a block
+      if (this.stateExists("fieldGoalLineBlitzed"))
+        return this.handleUnsuccessfulFg("Field goal blocked!");
+      return this.handleSuccessfulFg("Auto fg, defense kicked it");
+    }
+
+    // They blitzed the ball before the kick
+    this.setState("fieldGoalBlitzed");
+  }
+
+  private _handleBallContactKicker(ballContactObj: BallContact) {
+    if (this.stateExists("fieldGoalKicked"))
+      return this.handleUnsuccessfulFg("Illegal fg, touched by kicker");
+
+    if (ballContactObj.type === "touch") return;
+
+    // Now we know its a kick
+
+    // If they try to kick during a run, it counts as a blitz
+    if (this.stateExists("ballRan")) return this.setState("fieldGoalBlitzed");
+
+    this.setState("fieldGoalKicked");
+  }
+
+  protected _handleBallContactOffense(ballContactObj: BallContact): void {
+    const { player } = ballContactObj;
+
+    if (player.id === this._kicker.id)
+      return this._handleBallContactKicker(ballContactObj);
+
+    // If offense touches the ball at anytime, its an incomplete field goal
+
+    Chat.send("Illegal fg, touched by offense");
+    return this.endPlay({ resetDown: true });
+  }
+
+  protected _handleRun(playerContactObj: PlayerContact) {
+    const { player } = playerContactObj;
+
+    Chat.send(`${ICONS.Running} Ball Ran!`);
+
+    this.setBallCarrier(player).setState("ballRan");
+  }
+
+  protected _handleTackle(playerContactObj: PlayerContact) {
+    const { endPosition, netYards, yardAndHalfStr } = this._getPlayDataOffense(
+      playerContactObj.ballCarrierPosition
+    );
+
+    Room.game.stats.updatePlayerStat(playerContactObj.player.id, {
+      tackles: 1,
+    });
+
+    // Check for sack
+    const isSack =
+      GameReferee.checkIfSack(
+        playerContactObj.ballCarrierPosition,
+        Room.game.down.getLOS().x,
+        Room.game.offenseTeamId
+      ) && this.stateExists("ballRan") === false;
+
+    if (isSack) {
+      Chat.send(
+        `${ICONS.HandFingersSpread} ${playerContactObj.player.name} with the SACK!`
+      );
+
+      Room.game.stats.updatePlayerStat(playerContactObj.player.id, {
+        sacks: 1,
+      });
+
+      Room.game.stats.updatePlayerStat(playerContactObj.player.id, {
+        qbSacks: 1,
+      });
+    } else {
+      Chat.send(`${ICONS.HandFingersSpread} Tackle ${yardAndHalfStr}`);
+    }
+
+    // Tackle on a run, or the kicker just goes out of bounds
+    if (
+      this.stateExists("ballRan") ||
+      this._kicker.id === this._ballCarrier!.id
+    ) {
+      Room.game.stats.updatePlayerStat(this._ballCarrier!.id, {
+        rushingAttempts: 1,
+        rushingYards: netYards,
+      });
+    }
+
+    const { isSafety, isTouchback } =
+      GameReferee.checkIfSafetyOrTouchbackPlayer(
+        Room.game.down.getLOS(),
+        playerContactObj.ballCarrierPosition,
+        Room.game.offenseTeamId
+      );
+
+    if (isSafety) return this.handleSafety();
+    if (isTouchback) return this.handleTouchback();
+
+    this.endPlay({
+      newLosX: endPosition.x,
+      netYards,
+    });
+  }
+
+  handleIllegalCrossOffense() {
+    return this._handlePenalty("illegalLosCross", this._kicker);
+  }
+
+  onBallOutOfHashes() {
+    this.handleUnsuccessfulFg("Missed");
+  }
+
+  onKickDrag(): void {
+    this._handlePenalty("fgDrag", this._kicker);
+  }
 
   cleanUp(): void {}
-
-  // _kicker: PlayerObject;
-  // constructor(time: number, kicker: PlayerObject) {
-  //   super(time);
-  //   this._kicker = kicker;
-  //   this._ballCarrier = kicker;
-  // }
-  // getStartingPosition() {
-  //   return down.getLOS();
-  // }
-  // putKickerInPosition() {
-  //   const { team, id } = this._kicker;
-  //   const sevenYardsBehindBall = new DistanceCalculator([
-  //     down.getSnapDistance(),
-  //     MAP.YARD * 7,
-  //   ])
-  //     .subtractByTeam(team)
-  //     .getDistance();
-  //   room.setPlayerDiscProperties(id, {
-  //     x: sevenYardsBehindBall,
-  //     y: MAP_POINTS.TOP_HASH,
-  //   });
-  //   return this;
-  // }
-  // putOffenseInPosition() {
-  //   const { team } = this._kicker;
-  //   const { offensePlayers } = game.getOffenseDefensePlayers();
-  //   const opposingEndzone = getOpposingTeamEndzone(team);
-  //   const fiveYardsBeforeEndzone = new DistanceCalculator([
-  //     opposingEndzone,
-  //     MAP.YARD * 5,
-  //   ])
-  //     .subtractByTeam(team)
-  //     .getDistance();
-  //   offensePlayers.forEach(({ id }) => {
-  //     if (id === this.getBallCarrier().id) return;
-  //     room.setPlayerDiscProperties(id, { x: fiveYardsBeforeEndzone });
-  //   });
-  //   return this;
-  // }
-  // putDefenseInPosition() {
-  //   const team = game.getDefenseTeam();
-  //   const { defensePlayers } = game.getOffenseDefensePlayers();
-  //   const opposingEndzone = getOpposingTeamEndzone(game.getOffenseTeam());
-  //   const oneYardInFrontOfEndzone = new DistanceCalculator([
-  //     opposingEndzone,
-  //     MAP.YARD * 1,
-  //   ])
-  //     .subtractByTeam(team)
-  //     .getDistance();
-  //   defensePlayers.forEach(({ id }) => {
-  //     room.setPlayerDiscProperties(id, { x: oneYardInFrontOfEndzone });
-  //   });
-  //   return this;
-  // }
-  // onSuccessfulFieldGoal() {
-  //   sendPlayMessage({
-  //     type: PLAY_TYPES.FG_GOOD,
-  //   });
-  //   game.setLivePlay(false);
-  //   this.scorePlay(3, game.getOffenseTeam());
-  // }
-  // onUnsuccessfulFieldGoal() {
-  //   sendPlayMessage({
-  //     type: PLAY_TYPES.FG_INCOMPLETE,
-  //   });
-  //   this.endPlay({ isIncomplete: true });
-  // }
-  // onBallOutOfHashes() {
-  //   sendPlayMessage({
-  //     type: PLAY_TYPES.FG_INCOMPLETE,
-  //   });
-  //   this.endPlay({ isIncomplete: true });
-  // }
-  // onBallContact(ballContactObj) {
-  //   // We have to do this check AGAIN because playerOnkick is not an event listener, but a native listener
-  //   if (this.getState("ballRan") || this.getState("fieldGoalBlitzed")) return;
-  //   super.onBallContact(ballContactObj);
-  // }
-  // onPlayerContactOffense(contactObj) {
-  //   // If he touches
-  //   const { player, playerPosition, ballCarrierPosition } = contactObj;
-  //   // Verify that its a legal run
-  //   const isBehindQuarterBack = checkIfBehind(
-  //     playerPosition.x,
-  //     ballCarrierPosition.x,
-  //     player.team
-  //   );
-  //   if (isBehindQuarterBack) return this.handleRun(contactObj);
-  //   handlePenalty({
-  //     type: PENALTY_TYPES.ILLEGAL_RUN,
-  //     playerName: player.name,
-  //   });
-  // }
-  // onPlayerContactDefense(contactObj) {
-  //   const { player, playerPosition, ballCarrierPosition } = contactObj;
-  //   const { team, name } = this.getBallCarrier();
-  //   const { netYards, endPosition, endYard, mapSection } = super.getPlayData(
-  //     ballCarrierPosition,
-  //     team
-  //   );
-  //   const isSack = checkIfBehind(endPosition, down.getLOS(), team);
-  //   sendPlayMessage({
-  //     type: isSack ? PLAY_TYPES.SACK : PLAY_TYPES.TACKLE,
-  //     playerName: name,
-  //     player2Name: player.name,
-  //     yard: endYard,
-  //     netYards: netYards,
-  //     mapSection: mapSection,
-  //     position: endPosition,
-  //   });
-  //   maybeSendQuoteMessage(player.name, netYards);
-  //   const isSafety = checkIfSafetyPlayer(ballCarrierPosition, team);
-  //   if (isSafety) return super.handleSafety();
-  //   this.endPlay({ netYards: netYards, endPosition, endPosition });
-  // }
-  // onPlayerOutOfBounds(ballCarrierPosition) {
-  //   const { team, name } = this.getBallCarrier();
-  //   const { netYards, endPosition, endYard } = super.getPlayData(
-  //     ballCarrierPosition,
-  //     team
-  //   );
-  //   sendPlayMessage({
-  //     type: PLAY_TYPES.PLYR_OUT_OF_BOUNDS,
-  //     playerName: name,
-  //     yard: endYard,
-  //     netYards: netYards,
-  //     position: endPosition,
-  //   });
-  //   const isSafety = checkIfSafetyPlayer(ballCarrierPosition, team);
-  //   if (isSafety) return super.handleSafety();
-  //   this.endPlay({ netYards: netYards, endPosition: endPosition });
-  // }
-  // onKickDrag(dragAmount) {
-  //   Chat.send("DRAG ON KICK" + dragAmount);
-  //   handlePenalty({
-  //     type: PENALTY_TYPES.FG_DRAG,
-  //     playerName: this._kicker.name,
-  //   });
-  //   this.endPlay({ isIncomplete: true });
-  // }
-  // handleRun(contactObj) {
-  //   const { player, playerPosition } = contactObj;
-  //   sendPlayMessage({ type: PLAY_TYPES.RUN, playerName: player.name });
-  //   this.setBallCarrier(player).setState("ballRan");
-  // }
-  // handleIllegalCrossOffense() {
-  //   handlePenalty({
-  //     name: PENALTY_TYPES.ILLEGAL_LOS_CROSS,
-  //     playerName: this.getBallCarrier().name,
-  //   });
-  // }
-  // handleAutoIncomplete(playerName) {
-  //   sendPlayMessage({
-  //     type: PLAY_TYPES.FG_INCOMPLETE,
-  //     playerName: playerName,
-  //   });
-  //   this.endPlay({ isIncomplete: true });
-  // }
-  // handleBallContactDefense(ballContactObj) {
-  //   const {
-  //     player: { name },
-  //   } = ballContactObj;
-  //   if (this.getState("fieldGoalKicked") === false)
-  //     return this.setState("fieldGoalBlitzed");
-  //   sendPlayMessage({
-  //     type: PLAY_TYPES.FG_AUTO_GOOD,
-  //     playerName: name,
-  //   });
-  //   this.scorePlay(3, game.getOffenseTeam());
-  // }
-  // handleBallContactOffense(ballContactObj) {
-  //   const {
-  //     type,
-  //     player: { id, name },
-  //   } = ballContactObj;
-  //   if (id !== this._kicker.id || this.getState("fieldGoalKicked"))
-  //     return this.handleAutoIncomplete(name);
-  //   if (type === BALL_CONTACT_TYPES.KICK)
-  //     return this.setState("fieldGoalKicked");
-  // }
-  // endPlay({ isIncomplete = false, netYards, endPosition }) {
-  //   if (isIncomplete) {
-  //     play = null;
-  //     game.setLivePlay(false);
-  //     game.swapOffense();
-  //     setBallAndFieldMarkersPlayEnd();
-  //     setPlayers();
-  //     down.startNew();
-  //     sendDownAndDistance();
-  //     return;
-  //   }
-  //   super.endPlay({ netYards, endPosition });
-  // }
 }
--- src/room/plays/play_events/FieldGoal.events.ts
+++ src/room/plays/play_events/FieldGoal.events.ts
@@ -1,32 +1,109 @@
+import Room from "../..";
 import BallContact from "../../classes/BallContact";
 import PlayerContact from "../../classes/PlayerContact";
-import { Position } from "../../HBClient";
+import { PlayableTeamId, PlayerObjFlat, Position } from "../../HBClient";
+import Chat from "../../roomStructures/Chat";
+import Ball from "../../structures/Ball";
+import GameReferee from "../../structures/GameReferee";
+import MapReferee from "../../structures/MapReferee";
+import { quickPause } from "../../utils/haxUtils";
+import ICONS from "../../utils/Icons";
 import BasePlay from "../BasePlay";
 
 export interface FieldGoalStore {
   fieldGoal: true;
   fieldGoalKicked: true;
+  fieldGoalLineBlitzed: true;
   fieldGoalBlitzed: true;
+  ballRan: true;
 }
 
 export default abstract class FieldGoalEvents extends BasePlay<FieldGoalStore> {
+  protected abstract _getKicker(): PlayerObjFlat;
+  protected abstract _setPlayersInPosition(): void;
+  protected abstract _handleTackle(playerContactObj: PlayerContact): any;
+  protected abstract _handleRun(playerContact: PlayerContact): any;
+
   validateBeforePlayBegins() {
-    return {
-      valid: true,
-      message: "",
-      sendToPlayer: true,
-    };
-  }
-  prepare() {
-    // Room.game.updateStaticPlayers();
-    // this.setBallPositionOnSet(Ball.getPosition());
-    // this._startBlitzClock();
+    // No real validation
   }
 
-  run() {}
-  handleBallOutOfBounds(ballPosition: Position) {}
-  handleBallCarrierOutOfBounds(ballCarrierPosition: Position) {}
-  handleBallCarrierContactOffense(playerContact: PlayerContact) {}
-  handleBallCarrierContactDefense(playerContact: PlayerContact) {}
-  handleBallContact(ballContactObj: BallContact) {}
+  prepare() {
+    Room.game.updateStaticPlayers();
+    this._setStartingPosition(Room.game.down.getLOS());
+    this.setBallPositionOnSet(Ball.getPosition());
+    Room.game.down.moveFieldMarkers();
+    this._setPlayersInPosition();
+  }
+
+  run() {
+    this._setLivePlay(true);
+    Ball.release();
+    this.setState("fieldGoal");
+    Chat.sendMessageMaybeWithClock(
+      `${ICONS.PurpleCircle} Field Goal`,
+      this.time
+    );
+    quickPause();
+  }
+  handleBallOutOfBounds(ballPosition: Position) {
+    // This actually will never run since we stop play when the ball leaves the hashes
+  }
+  handleBallCarrierOutOfBounds(ballCarrierPosition: Position) {
+    // Out of bounds like always, check for safety etc
+    const { endPosition, netYards, yardAndHalfStr } =
+      this._getPlayDataOffense(ballCarrierPosition);
+
+    Chat.send(
+      `${ICONS.Pushpin} ${
+        this.getBallCarrier().name
+      } went out of bounds ${yardAndHalfStr}`
+    );
+
+    const { isSafety, isTouchback } =
+      GameReferee.checkIfSafetyOrTouchbackPlayer(
+        Room.game.down.getLOS(),
+        ballCarrierPosition,
+        Room.game.offenseTeamId
+      );
+
+    // Always add rushing stats
+    Room.game.stats.updatePlayerStat(this._ballCarrier?.id!, {
+      rushingAttempts: 1,
+      rushingYards: netYards,
+    });
+
+    if (isSafety) return this.handleSafety();
+    if (isTouchback) return this.handleTouchback();
+
+    this.endPlay({ newLosX: endPosition.x, netYards });
+  }
+  handleBallCarrierContactOffense(playerContact: PlayerContact) {
+    const { player, playerPosition, ballCarrierPosition } = playerContact;
+
+    const isBehindKicker = MapReferee.checkIfBehind(
+      playerPosition.x,
+      ballCarrierPosition.x,
+      player.team as PlayableTeamId
+    );
+
+    // If its a legal run, handle it, otherwise its a penalty
+    if (isBehindKicker) return this._handleRun(playerContact);
+
+    // Cant run when the ball has already been kicked
+    if (this.stateExists("fieldGoalKicked")) return;
+
+    this._handlePenalty("illegalRun", player);
+  }
+  handleBallCarrierContactDefense(playerContact: PlayerContact) {
+    this._handleTackle(playerContact);
+  }
+
+  handleBallContact(ballContactObj: BallContact) {
+    // We have to do this check AGAIN because playerOnkick is not an event listener, but a native listener
+    if (this.stateExists("ballRan") || this.stateExists("fieldGoalBlitzed"))
+      return;
+
+    super.handleBallContact(ballContactObj);
+  }
 }
