a06d8e761313e0d02d6f3ebea77cf7f10bdb4e86
Phantomat0 <soldierxfreedom@gmail.com>
2022-05-26 23:04:05 -0500

    updatestatic players on player leave

--- src/room/events/onLeave.ts
+++ src/room/events/onLeave.ts
@@ -5,4 +5,6 @@ export default function onLeave(player: PlayerObject) {
   Room.players.delete(player);
 
   if (!Room.isBotOn || !Room.game) return;
+
+  Room.game.players.updateStaticPlayerList(Room.game.offenseTeamId);
 }
