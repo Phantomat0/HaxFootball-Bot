78b4328fcf8e535c0b62fad6959c1c3e184ecc6f
Phantomat0 <soldierxfreedom@gmail.com>
2022-06-03 00:14:50 -0500

    whats new greeting message

--- src/room/events/onJoin.ts
+++ src/room/events/onJoin.ts
@@ -1,9 +1,16 @@
 import Room, { client } from "..";
 import { FullPlayerObject } from "../HBClient";
 import { SHOW_DEBUG_CHAT } from "../roomConfig";
+import Chat from "../roomStructures/Chat";
+import COLORS from "../utils/colors";
 
 export default function onJoin(player: FullPlayerObject) {
   Room.players.createAndAdd(player);
+
+  Chat.send(
+    `Whats new: Passer Rating, QB can move ball anywhere behind LOS, defense can blitz 3 seconds after ball has been moved`,
+    { id: player.id, color: COLORS.LightBlue }
+  );
 
   if (!Room.isBotOn) return;
 
