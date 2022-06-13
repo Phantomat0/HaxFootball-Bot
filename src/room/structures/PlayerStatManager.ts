366ae2f5a4c9a53360698930117ea05af818f235
Phantomat0 <soldierxfreedom@gmail.com>
2022-06-13 13:51:00 -0500

    get man of the match

--- src/room/structures/PlayerStatManager.ts
+++ src/room/structures/PlayerStatManager.ts
@@ -41,4 +41,90 @@ export default class PlayerStatManager {
 
     this.statsCollection.get(playerProfile.auth)?.updateStats(statQuery);
   }
+
+  determineManOfTheMatch() {
+    const POINT_PER_STAT_MAP: Partial<Record<keyof PlayerStats, number>> = {
+      // Receiving
+      totalReceptions: 1,
+      totalReceivingYards: 0.1,
+      rushingYards: 1,
+      touchdownsReceived: 6,
+      touchdownsRushed: 6,
+
+      // Passing
+      totalPassAttempts: 0,
+      totalPassCompletions: 1,
+      totalPassYards: 0.04,
+      touchdownsThrown: 4,
+      interceptionsThrown: -12,
+      qbSacks: 0,
+
+      // Defense
+      totalPassDeflections: 5,
+      tackles: 3,
+      totalYardsAllowed: 0,
+      sacks: 6,
+      forcedFumbles: 0,
+      interceptionsReceived: 12,
+
+      // Misc
+      penalties: -10,
+    };
+
+    const playersStatsWithPoints = this.statsCollection
+      .find()
+      .map((statsObj) => {
+        const statsPoints = Object.entries(
+          Object.getOwnPropertyDescriptors(statsObj)
+        ).reduce(
+          (
+            acc: Partial<Record<keyof typeof POINT_PER_STAT_MAP, number>>,
+            propKeyDescriptor
+          ) => {
+            const [statName, keyValue] = propKeyDescriptor;
+
+            const statValue =
+              (keyValue.value || (keyValue.get && keyValue.get())) ?? 0;
+
+            if (statName in POINT_PER_STAT_MAP === false) return acc;
+
+            const statPointValue: number = POINT_PER_STAT_MAP[statName];
+
+            acc[statName] = statValue * statPointValue;
+
+            return acc;
+          },
+          {}
+        );
+
+        return { playerAuth: statsObj.player.auth, statsPoints };
+      });
+
+    // Ok now just add up the points
+
+    const eachPlayersPoints = playersStatsWithPoints.map((playerStatObj) => {
+      const pointTotal = Object.values(playerStatObj.statsPoints).reduce(
+        (a, b) => a + b
+      );
+      return {
+        playerAuth: playerStatObj.playerAuth,
+        pointTotal,
+      };
+    });
+
+    // Alas, now lets find the player with the highest point total and return his auth
+
+    const playerWithMostPoints = eachPlayersPoints.reduce(
+      (prev, current) =>
+        prev.pointTotal > current.pointTotal ? prev : current,
+      { playerAuth: "", pointTotal: 0 }
+    );
+
+    if (playerWithMostPoints.pointTotal === 0) return null;
+
+    return {
+      auth: playerWithMostPoints.playerAuth,
+      pointTotal: playerWithMostPoints.pointTotal,
+    };
+  }
 }
