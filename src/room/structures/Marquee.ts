20148dea7a029556ccd31444a90457087ec04141
Phantomat0 <soldierxfreedom@gmail.com>
2022-06-13 13:51:11 -0500

    marquee to post announcements every 2 mins

---
+++ src/room/structures/Marquee.ts
@@ -0,0 +1,18 @@
+import Chat from "../roomStructures/Chat";
+import { randFromArray } from "../utils/utils";
+
+const ANNOUNCEMENTS: string[] = [
+  "!stats or !stats partialplayername to view your stats or that of another player",
+  "You can rush the field goal kicker before he kicks the fieldgoal",
+];
+
+export default class Marquee {
+  static run() {
+    const TWO_MINUTES_IN_MS = 140000;
+    setInterval(async () => {
+      const announcement = randFromArray(ANNOUNCEMENTS);
+
+      Chat.send(announcement, { sound: 0 });
+    }, TWO_MINUTES_IN_MS);
+  }
+}
