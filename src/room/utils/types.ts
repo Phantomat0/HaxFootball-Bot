99672cd1409fc6afa003ed309c10780070e44413
Phantomat0 <soldierxfreedom@gmail.com>
2022-06-21 21:19:15 -0500

    haxball types

--- src/room/utils/types.ts
+++ src/room/utils/types.ts
@@ -1,3 +1,13 @@
-export type RequiredKeys<T, K extends keyof T> = T & Required<Pick<T, K>>;
+import { TeamId } from "../HBClient";
 
-export type ValueOf<T> = T[keyof T];
+interface TeamIdEnum {
+  SPECTATORS: TeamId;
+  RED: TeamId;
+  BLUE: TeamId;
+}
+
+export const TEAMS: TeamIdEnum = {
+  SPECTATORS: 0,
+  RED: 1,
+  BLUE: 2,
+};
