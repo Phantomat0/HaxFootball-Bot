70b070b90d869807f61f5c3b34b4ff735ad310f9
Phantomat0 <soldierxfreedom@gmail.com>
2022-08-17 10:33:04 -0500

    roomConfig template

---
+++ src/room/roomConfig.ts
@@ -0,0 +1,15 @@
+import { HBClientConfig } from "./HBClient";
+
+const roomConfig: HBClientConfig = {
+  roomName: "🏈 HAXFOOTBALL 🏈",
+  maxPlayers: 14,
+  //   password: ""
+  noPlayer: true,
+  public: false,
+  token: "thr1.AAAAAGL9AZEuSxhVEssujQ.zFaAnDPk-Ao",
+};
+
+// For testing purposes
+export const SHOW_DEBUG_CHAT: boolean = false;
+
+export default roomConfig;
