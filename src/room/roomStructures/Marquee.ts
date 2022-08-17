4244f443dd054cf21cb5819c178606b1db59a3bd
Phantomat0 <soldierxfreedom@gmail.com>
2022-08-17 10:37:36 -0500

    weighted marquee text

--- src/room/roomStructures/Marquee.ts
+++ src/room/roomStructures/Marquee.ts
@@ -1,19 +1,34 @@
 import Chat from "./Chat";
-import { randFromArray } from "../utils/utils";
+import { randFromArrayWeighted } from "../utils/utils";
 
-const ANNOUNCEMENTS: string[] = [
-  "!stats or !stats partialplayername to view your stats or that of another player",
-  "You can rush the field goal kicker before he kicks the fieldgoal",
-  "Join our Discord discord.gg/VdrD2p7",
+interface Announcement {
+  text: string;
+  weight: number;
+}
+
+const ANNOUNCEMENTS: Announcement[] = [
+  {
+    text: `!stats or !stats partialplayername to view your stats or that of another player`,
+    weight: 0.3,
+  },
+  {
+    text: `!rules2 for more indepth rules!`,
+    weight: 0.3,
+  },
+  {
+    text: `Join our Discord discord.gg/VdrD2p7`,
+    weight: 0.3,
+  },
 ];
 
 export default class Marquee {
   static run() {
     const TWO_MINUTES_IN_MS = 140000;
     setInterval(async () => {
-      const announcement = randFromArray(ANNOUNCEMENTS);
+      const announcementObj = randFromArrayWeighted(ANNOUNCEMENTS);
 
-      Chat.send(announcement, { sound: 0 });
+      const announcementText = announcementObj.text;
+      Chat.send(announcementText, { sound: 0 });
     }, TWO_MINUTES_IN_MS);
   }
 }
