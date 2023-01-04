187725fd718103812b8a8b85e0229d670271fc42
Phantomat0 <soldierxfreedom@gmail.com>
2023-01-04 00:35:55 -0600

    make TEAMS a const, and add TeamsObj with team names

--- src/room/utils/types.ts
+++ src/room/utils/types.ts
@@ -1,13 +1,20 @@
-import { TeamId } from "../HBClient";
-
-interface TeamIdEnum {
-  SPECTATORS: TeamId;
-  RED: TeamId;
-  BLUE: TeamId;
-}
-
-export const TEAMS: TeamIdEnum = {
+export const TEAMS = {
   SPECTATORS: 0,
   RED: 1,
   BLUE: 2,
-};
+} as const;
+
+export const TEAM_OBJ = {
+  SPECTATORS: {
+    id: 0,
+    name: "Spectators",
+  },
+  RED: {
+    id: 1,
+    name: "Red",
+  },
+  BLUE: {
+    id: 2,
+    name: "Blue",
+  },
+} as const;
